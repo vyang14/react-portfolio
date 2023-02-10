2efbc7cdc37215e53a8dd5cfe06df141db76876b
vyang14 <vyang1014888@gmail.com>
2023-02-09 21:29:53 -0500

    adding components and framer containers

--- src/App.js
+++ src/App.js
@@ -1,1 +1,2 @@
 import React from 'react';
+import Navbar from './components/Navbar/Navbar';
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Footer.css';
+
+export default function Footer() {
+    return (
+        <>
+            <footer id="footer">
+                <p>Vincent Yang 2023</p>
+            </footer>
+        </>
+    )
+}
